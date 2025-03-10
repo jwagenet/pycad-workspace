027b95946f31b86da744d2fe74326c6ed1b091ba
Jonathan Wagenet <jonathanwagenet@gmail.com>
2025-03-09 21:57:58 -0400

    Use new objects for AubergineSlot

--- mixins/objects_tangent.py
+++ mixins/objects_tangent.py
@@ -276,94 +276,19 @@ class AubergineSlot(BaseSketchObject):
         start_point = Vector(0, 0)
         end_point = Vector(length, 0)
 
-        center_side = 1
-        if side == Side.RIGHT:
-            center_side = -1
+        center_side = 1 if side == Side.LEFT else -1
 
-        with BuildSketch() as sketch:
-            with BuildLine():
-                with BuildLine(mode=Mode.PRIVATE):
-                    # The normal could be Axis.Y if end_point.Y is 0
-                    # originally end_point could be an arbitrary point
-                    # keeping for now
-                    midline = Line(start_point, end_point)
-                    normal = center_side * Vector(end_point.Y, -end_point.X)
+        c1 = CenterArc(start_point, start_radius, start_angle=0, arc_size=360)
+        c2 = CenterArc(end_point, end_radius, start_angle=0, arc_size=360)
 
-                    # Method:
-                    # - the centerpoint of the inner arc is found by the intersection of the
-                    #   arcs made by adding the inner radius to the point radii
-                    # - the centerpoint of the outer arc is found by the intersection of the
-                    #   arcs made by subtracting the outer radius from the point radii
-                    # then its a matter of finding the points where the connecting lines
-                    # intersect the point circles
+        a1 = ArcArcTangentArc(c1, c2, inner_radius, side=side, keep=Keep.INSIDE)
+        a2 = ArcArcTangentArc(c1, c2, outer_radius, side=side, keep=Keep.OUTSIDE)
+        a3 = TangentArc([a1 @ 0, a2 @ 0], tangent=center_side * (a1 % 0))
+        a4 = TangentArc([a1 @ 1, a2 @ 1], tangent=center_side * -(a1 % 1))
 
-                    c1 = CenterArc(start_point, start_radius, start_angle=0, arc_size=360)
-                    c13 = CenterArc(start_point, start_radius + inner_radius, start_angle=0, arc_size=360)
-                    c14 = CenterArc(start_point, outer_radius - start_radius, start_angle=0, arc_size=360)
-                    c2 = CenterArc(end_point, end_radius, start_angle=0, arc_size=360)
-                    c23 = CenterArc(end_point, end_radius + inner_radius, start_angle=0, arc_size=360)
-                    c24 = CenterArc(end_point, outer_radius - end_radius, start_angle=0, arc_size=360)
+        face = Face(Wire.combine([a1, a2, a3, a4]))
 
-                    # perform checks on validity of inner and outer radii
-                    inner_radius_length = inner_radius + (start_radius + end_radius) / 2
-                    outer_radius_length = outer_radius - (start_radius + end_radius) / 2
-
-                    if inner_radius_length <= midline.length / 2:
-                        raise ValueError(f"The inner arc radius is too small. Should be greater than {(midline.length - (start_radius + end_radius)) / 2} (and probably larger).")
-
-                    if outer_radius_length <= midline.length / 2:
-                        raise ValueError(f"The outer arc radius is too small. Should be greater than {(midline.length + (start_radius + end_radius)) / 2} (and probably larger).")
-
-                    # xs* can have no intersections as some small amount
-                    # can also only have 1 vertex
-                    # workaround to catch error center_spacing / average radius needs to be less than 1.888 or greater than .66 from testing
-                    max_ratio = 1.888
-                    min_ratio = .666
-
-                    if midline.length / inner_radius_length < min_ratio:
-                        raise ValueError(f"The inner arc radius is too large. Should be less than {(midline.length / min_ratio - (start_radius + end_radius) / 2)}.")
-
-                    if midline.length / inner_radius_length > max_ratio:
-                        raise ValueError(f"The inner arc radius is too small. Should be greater than {(midline.length / max_ratio - (start_radius + end_radius) / 2)}.")
-
-                    if midline.length / outer_radius_length < min_ratio:
-                        raise ValueError(f"The outer arc radius is too large. Should be less than {(midline.length / min_ratio + (start_radius + end_radius) / 2)}.")
-
-                    if midline.length / outer_radius_length > max_ratio:
-                        raise ValueError(f"The outer arc radius is too small. Should be greater than {(midline.length / max_ratio + (start_radius + end_radius) / 2)}.")
-
-                    # check we get intersections anyway
-                    xsa = c13.edge().intersect(c23.edge())
-                    xsb = c14.edge().intersect(c24.edge())
-
-                    if xsa is None:
-                        raise ValueError("Could not find intersections. Try larger or smaller inner_radius")
-
-                    if xsb is None:
-                        raise ValueError("Could not find intersections. Try larger or smaller outer_radius")
-
-                    # find intersections and tangents
-                    xa = xsa.sort_by(Axis(start_point, normal))[0]
-                    xb = xsb.sort_by(Axis(start_point, normal))[0]
-
-                    ia1 = IntersectingLine(start_point, Vector(xa), c1)
-                    ib1 = IntersectingLine(start_point, -Vector(xb), c1)
-                    ia2 = IntersectingLine(end_point, (Vector(xa) - Vector(end_point)), c2)
-                    ib2 = IntersectingLine(end_point, (-Vector(xb) + Vector(end_point)), c2)
-
-                    ta1 = Vector((ia1 % 1).Y, -(ia1 % 1).X)
-                    ta2 = Vector((ia2 % 1).Y, -(ia2 % 1).X)
-                    tb1 = Vector((ib1 % 1).Y, -(ib1 % 1).X)
-
-                a1x = TangentArc([ia1 @ 1, ib1 @ 1], tangent=center_side * -ta1)
-                a2x = TangentArc([ia1 @ 1, ia2 @ 1], tangent=center_side * ta1)
-                a3x = TangentArc([ia2 @ 1, ib2 @ 1], tangent=center_side * ta2)
-                a4x = TangentArc([ib1 @ 1, ib2 @ 1], tangent=center_side * -tb1)
-
-            make_face()
-
-        # removed align option because its a little weird
-        super().__init__(obj=sketch.sketch, rotation=rotation, mode=mode)
+        super().__init__(obj=face, rotation=rotation, mode=mode)
 
 
 class AubergineArcSlot(BaseSketchObject):
